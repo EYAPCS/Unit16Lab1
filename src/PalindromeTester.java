fb851c1c550624f87ffb030cbf01afc084e3454d
Emma <emmayang78@gmail.com>
2017-07-19 14:30:37 +0800

    Added comments

--- src/PalindromeTester.java
+++ src/PalindromeTester.java
@@ -3,6 +3,11 @@ import java.util.Arrays;
 /**
  * Created by emma on 7/18/17.
  */
+
+/**
+ * Creates a recursive method that tests whether a String expressed as an array of Strings is a palindrome.
+ */
+
 public class PalindromeTester {
 
     public static boolean testPalindrome (String[] str) {
@@ -21,7 +26,7 @@ public class PalindromeTester {
 
     public static void main(String[] args) {
 
-        System.out.println(testPalindrome(new String[]{}));
+        System.out.println(testPalindrome(new String[]{"a", "m", "a", "n", "a", "m", "a"}));
 
     }
 
